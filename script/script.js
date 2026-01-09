0e91f4ba7e936a7625b82a54450304d2ec1ad6cd
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-01-09 16:27:27 +0100

    Push di aggiornamento, aggiunta di titoli, immagini e date tramite chiamata axios all'endpoint fornito.

--- script/script.js
+++ script/script.js
@@ -1,21 +1,32 @@
 const body = document.getElementById("body");
 const cardsContainer = document.getElementById("CardsContainerID");
-const cardBackgroundImg = "https://www.color-hex.com/palettes/28997.png"
+const cardBackgroundEndpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
-for (let index = 0; index < 6; index++) {
 
-    
-    
-cardsContainer.innerHTML += 
-            `<div class="card py-0 px-3">
-                <img class="Pin shadow-lg" src="./img/pin.svg" alt="">
-                <div class="CardImage d-flex align-items-center justify-content-center justify-content-center" style="background-image: url(${cardBackgroundImg});">
-                    600x600
+
+for (let i = 0; i < 6; i++) {
+
+
+    axios.get(cardBackgroundEndpoint)
+        .then(response => {
+
+            let responseOne = response.data;
+
+            
+
+            cardsContainer.innerHTML +=
+                `<div class="card py-0 px-3">
+                <img class="Pin shadow" src="./img/pin.svg" alt="">
+                <div class="CardImage d-flex align-items-center justify-content-center justify-content-center" style="background-image: url(${responseOne[i].url})">
                 </div>
-                <div class="CardText py-2">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                </div>
+                <div class="CardTitle py-2">${responseOne[i].title}</div>
+                <div class="CardDate py-2">${responseOne[i].date}</div>
             </div>`;
-    
-}
 
+        
+    })
+
+    .catch (error => {
+    console.error(error.message)
+})
+}
